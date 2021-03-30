34a86b50fbfe7a7474e97703d59ae80f8001ded5
Cedric Chee <cedric@invictusbyte.com>
2021-03-30 08:24:29 +0000

    use return value from new class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -23,12 +23,12 @@ class Bottles
   end
 
   def container(number)
-    BottleNumber.new(number).container(number)
     if number == 1
       "bottle"
     else
       "bottles"
     end
+    BottleNumber.new(number).container(number)
   end
 
   def action(number)
